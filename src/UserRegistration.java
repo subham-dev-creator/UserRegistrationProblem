6352970b7d79412f12b435178dd80866a4cb1297
subhamstark <subhamofficial98@gmail.com>
2020-09-27 22:12:09 +0530

    UC1 Done

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("USER REGISTRATION PROBLEM");
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the first Name : ");
+        String fName=sc.next();
+
+        String pattern = "[A-Z]{1}[a-z]{2,}";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(fName);
+        if(m.matches()) {
+            System.out.println("Valid First Name");
+        }
+        else{
+            System.out.println("Invalid First Name");
+        }
+    
     }
 }
