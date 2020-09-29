96a16aa47a6f687d3fded60e245641a884b16c44
Madhu Yadav <subhamofficial98@gmail.com>
2020-09-29 23:35:34 +0530

    Uc 9 Done

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -3,13 +3,6 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
-    static Boolean flNameValidation(String s){
-        String pattern = "[A-Z]{1}[a-z]{2,}";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(s);
-        if(m.matches()) return  true;
-        else return false;
-    }
 
     static Boolean emailValidation(String s){
         String pattern = "\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b";
@@ -20,64 +13,29 @@ public class UserRegistration {
         else return false;
     }
 
-
-    static Boolean mobNumValidation(String s){
-        String pattern="[0-9]{2}+[0-9]{10}";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(s);
-
-        if(m.matches()) return  true;
-        else return false;
-    }
-
-    static Boolean passValidation(String s){
-        String pattern="(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(s);
-
-        if(m.matches()) return true;
-        else return false;
-    }
-
     public static void main(String[] args) {
+        System.out.println(emailValidation("abc@yahoo.com"));
+        System.out.println(emailValidation("abc-100@yahoo.com"));
+        System.out.println(emailValidation("abc.100@yahoo.com"));
+        System.out.println(emailValidation("abc111@abc.com"));
+        System.out.println(emailValidation("abc-100@abc.net"));
+        System.out.println(emailValidation("abc.100@abc.com.au"));
+        System.out.println(emailValidation("abc@1.com "));
+        System.out.println(emailValidation("abc@gmail.com.com"));
+        System.out.println(emailValidation("abc+100@gmail.com"));
+        System.out.println(emailValidation("abc"));
+        System.out.println(emailValidation("abc@.com.my"));
+        System.out.println(emailValidation("abc123@gmail.a"));
+        System.out.println(emailValidation("abc123@.com"));
+        System.out.println(emailValidation("abc123@.com.com"));
+        System.out.println(emailValidation(".abc@abc.com"));
+        System.out.println(emailValidation("abc()*@gmail.com"));
+        System.out.println(emailValidation("abc@%*.com"));
+        System.out.println(emailValidation("abc..2002@gmail.com"));
+        System.out.println(emailValidation("abc@abc@gmail.com"));
+        System.out.println(emailValidation("abc@gmail.com.1a"));
+        System.out.println(emailValidation("abc@gmail.com.aa.au"));
 
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("Enter the first Name : ");
-        String fName=sc.next();
-        System.out.println("Enter the last Name : ");
-        String lName=sc.next();
-        System.out.println("Enter the Email : ");
-        String email=sc.next();
-        System.out.println("Enter the Mobile Number : ");
-        String Mob=sc.next();
-        System.out.println("Enter the Password : ");
-        String pass=sc.next();
-
-        if(flNameValidation(fName))
-            System.out.println("Valid First Name");
-        else
-            System.out.println("Invalid First Name");
-
-        if(flNameValidation(lName))
-            System.out.println("Valid Last Name");
-        else
-            System.out.println("Invalid Last Name");
-
-        if(emailValidation(email))
-            System.out.println("Valid Email");
-        else
-            System.out.println("Invalid Email");
-
-        if(mobNumValidation(Mob))
-            System.out.println("Valid Mobile NUmber ");
-        else
-            System.out.println("Invalid Mobile Number ");
-
-        if(passValidation(pass))
-            System.out.println("Valid Password ");
-        else
-            System.out.println("Invalid Password ");
 
     }
 }
